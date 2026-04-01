db1e137fcbe0cf9b8bbe3fdbeec26d4280b82b3d
Max Brunsfeld <maxbrunsfeld@gmail.com>
2026-04-01 13:45:03 -0700

    WIP

--- crates/workspace/src/workspace.rs
+++ crates/workspace/src/workspace.rs
@@ -1919,7 +1919,7 @@ impl Workspace {
                         });
                         match open_mode {
                             OpenMode::Replace => {
-                                multi_workspace.replace(workspace.clone(), &*window, cx);
+                                multi_workspace.replace(workspace.clone(), window, cx);
                             }
                             OpenMode::Activate => {
                                 multi_workspace.activate(workspace.clone(), window, cx);
